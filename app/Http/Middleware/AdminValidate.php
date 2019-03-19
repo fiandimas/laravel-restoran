a6d04e04181cdca6b317776e3eb2945a18d5f9f1
fiandimas <kidaliez@gmail.com>
2019-03-19 16:24:24 +0700

    create middleware AdminValidate

---
+++ app/Http/Middleware/AdminValidate.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Session;
+
+class AdminValidate
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+      if(Session::get('login')){
+        if(Session::get('id_level') == 1){
+          return $next($request);
+        }else{
+          \abort(403,'Unauthorized action.');
+        }  
+      }else{
+        return redirect('/login');
+      }    
+    }
+}
